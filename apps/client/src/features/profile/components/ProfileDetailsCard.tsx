0214c4b98d0a7e4d1e79abe3e6fe2b00a9162adf
joehannaC <jhnnacansino@gmail.com>
2026-06-10 13:40:06 +0800

    Add required mark

--- apps/client/src/features/profile/components/ProfileDetailsCard.tsx
+++ apps/client/src/features/profile/components/ProfileDetailsCard.tsx
@@ -13,6 +13,7 @@ import {
     type UserGender,
 } from '../../../../../shared/types/enums.types';
 import { ChevronDown } from 'lucide-react';
+import RequiredMark from '../../../components/ui/RequiredMark.tsx';
 
 type ProfileDetailsCardProps = {
     profile: Profile;
