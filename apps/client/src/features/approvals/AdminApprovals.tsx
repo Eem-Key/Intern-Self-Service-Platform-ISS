36ae856ab5522fb488dffa311b936e2d4cae4b40
joehannaC <jhnnacansino@gmail.com>
2026-06-19 13:36:40 +0800

    Approval banner

--- apps/client/src/features/approvals/AdminApprovals.tsx
+++ apps/client/src/features/approvals/AdminApprovals.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import AdminSidebar from '../AdminSidebar';
+import Banner from '../approvals/banner/ApprovalBanner';
 
 function AdminApprovals() {
 
@@ -9,9 +10,16 @@ function AdminApprovals() {
     
             
     return (
-        <main className="flex min-h-screen items-center justify-center bg-[#eeeeee]">
-            <AdminSidebar />
-        <h1 className="text-2xl font-bold text-black">Approvals</h1>
+        <main className="min-h-screen bg-[#eeeeee] text-black lg:flex">
+        <AdminSidebar />
+
+        <section className="w-full px-4 pb-4 pt-20 sm:px-5 sm:pt-24 lg:ml-[270px] lg:px-6 lg:py-5">
+        {/*<section className="flex min-h-screen w-full px-4 py-4 sm:px-5 lg:ml-[270px] lg:px-6 lg:py-5">*/}
+            <div className="mx-auto flex min-h-full w-full max-w-[2560px] flex-col">
+            <Banner />
+
+            </div>
+        </section>
         </main>
     );
 }
---
+++ apps/client/src/features/approvals/banner/ApprovalBanner.tsx
@@ -0,0 +1,18 @@
+function Banner() {
+    return (
+        <div className="relative w-full overflow-hidden rounded-xl bg-[#002D6F] px-5 py-6 shadow-md sm:px-7 sm:py-7 lg:px-8 xl:py-8">
+            
+        <div className="relative z-10 max-w-[820px]">
+            <h1 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl xl:text-6xl">
+            Approvals
+            </h1>
+
+            <p className="mt-2 max-w-[1500px] text-xs leading-snug text-white sm:text-sm lg:text-base xl:text-lg">
+            Review and manage pending end-of-day reports, leave requests, and profile updates for approval.
+            </p>
+        </div>
+        </div>
+    );
+}
+
+export default Banner;
